70aae60c75343fece7d318552229638543a587c5
Andrew Cachia <andcachia@gmail.com>
2026-02-13 15:16:13 +0100

    Reverted previous commit

--- src/ChimeraExchangeToken.sol
+++ src/ChimeraExchangeToken.sol
@@ -2,6 +2,7 @@
 // Compatible with OpenZeppelin Contracts ^5.5.0
 pragma solidity 0.8.27;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract ChimeraExchangeToken is ERC20Permit {
